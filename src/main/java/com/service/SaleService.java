aad97c9525236cde5b520ba7f6fa8b6991ac0281
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-03-15 19:03:33 -0400

    feat(SaleService)

---
+++ src/main/java/com/service/SaleService.java
@@ -0,0 +1,80 @@
+package com.service;
+
+import com.DB.Database;
+import com.model.Sale;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Cristian
+ */
+public class SaleService extends Database {
+    
+    public Sale getSale(int id) throws SQLException {
+        String sql = "SELECT * FROM sale WHERE id = ?";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        result = statement.executeQuery();
+        Sale sale = null;
+        if(result.next())
+            sale = new Sale(result.getInt("id"), result.getInt("sale_invoice_id"),result.getInt("product_id"),
+                            result.getInt("quantity"), result.getBigDecimal("amount"));
+        closeConnection();
+        return sale;
+    }
+    
+    public List<Sale> getSalesByInvoice(int saleInvoiceId) throws SQLException {
+        String sql = "SELECT * FROM sale WHERE sale_invoice_id = ?";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setInt(1, saleInvoiceId);
+        result = statement.executeQuery();
+        List<Sale> sales = new ArrayList<>();
+        while(result.next())
+            sales.add(new Sale(result.getInt("id"), result.getInt("sale_invoice_id"),result.getInt("product_id"),
+                                result.getInt("quantity"), result.getBigDecimal("amount")));
+        closeConnection();
+        return sales;
+    }
+    
+    public void createSales(List<Sale> sales) throws SQLException {
+        String sql = "INSERT INTO sale(quantity, amount, product_id, sale_invoice_id) VALUES (?,?,?,?)";
+        statement = connection.prepareStatement(sql);
+        for(Sale sale: sales) {
+            statement.setInt(1, sale.getQuantity());
+            statement.setBigDecimal(2, sale.getAmount());
+            statement.setInt(3, sale.getProductId());
+            statement.setInt(4, sale.getSaleInvoiceId());
+            statement.addBatch();
+        }
+        statement.executeBatch();
+        connection.commit();
+        closeConnection();
+    }
+    
+    public void updateSales(List<Sale> sales) throws SQLException {
+        String sql = "UPADATE sale SET quantity = ?, amount = ? WHERE id = ?";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        for(Sale sale: sales) {
+            statement.setInt(1, sale.getQuantity());
+            statement.setBigDecimal(2, sale.getAmount());
+            statement.setInt(3, sale.getId());
+            statement.addBatch();
+        }
+        statement.executeBatch();
+        closeConnection();
+    }
+    
+    public void removeSale(int id) throws SQLException {
+        String sql = "DELETE FROM sale WHERE id = ?";
+        applyConnection();
+        statement = connection.prepareStatement(sql);
+        statement.setInt(1, id);
+        statement.executeUpdate();
+        closeConnection();
+    }
+}
