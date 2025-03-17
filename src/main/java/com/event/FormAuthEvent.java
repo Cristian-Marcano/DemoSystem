8fc94cf371ad7c7cf5f97e05fbd567fb5fbb7bb2
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-03-17 17:47:55 -0400

    feat(FormAuthEvent)

---
+++ src/main/java/com/event/FormAuthEvent.java
@@ -0,0 +1,14 @@
+package com.event;
+
+/**
+ *
+ * @author Cristian
+ */
+public interface FormAuthEvent {
+    
+    public void onLogin(String username, String password) throws Exception;
+    
+    public void onSignUp(String username, String password, String firstName, String lastName, String ci, String phone);
+    
+    public void onForgot(String username, String firstName, String lastName, String ci) throws Exception;
+}
